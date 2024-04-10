a84686e351782d5bc75c174939425682a4f695d1
FazioNico <contact@nicolasfazio.ch>
2024-04-10 02:20:00 +0200

    refactor: add src to ignore

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,11 @@
-import { FirebaseOptions } from "firebase/app";
+import { FirebaseOptions } from 'firebase/app';
 
 export const parseApiKey = (hex: string) => {
-  try {
-    // converte hex string to utf-8 string
-    const json = Buffer.from(hex, "hex").toString("utf-8");
-    const apiKey  = JSON.parse(json);
-    return apiKey as FirebaseOptions;
-  } catch (error) {
-    throw new Error("Invalid API key");
-  }
-}
+	// converte hex string to utf-8 string
+	if (!hex || hex.length <= 0) {
+		throw new Error('Unexisting API key');
+	}
+	const json = Buffer.from(hex, 'hex').toString('utf-8');
+	const apiKey = JSON.parse(json);
+	return apiKey as FirebaseOptions;
+};
