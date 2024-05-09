d147ea80fda5fb987a30736ad020dde305f5cbef
FazioNico <contact@nicolasfazio.ch>
2024-05-10 01:29:17 +0200

    chore: add config for wasm

--- vite.config.ghpages.ts
+++ vite.config.ghpages.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import inject from '@rollup/plugin-inject';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [libInjectCss()],
+	plugins: [wasm(), topLevelAwait(), libInjectCss()],
 	base: '/firebase-web3connect/',
 	build: {
 		target: 'esnext',
