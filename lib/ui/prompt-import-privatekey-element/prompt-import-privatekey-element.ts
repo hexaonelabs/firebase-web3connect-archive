218c010abffc12528f85713afe0a26334d2efe44
FazioNico <contact@nicolasfazio.ch>
2024-05-21 15:59:52 +0200

    refactor: typo

--- lib/ui/prompt-import-privatekey-element/prompt-import-privatekey-element.ts
+++ lib/ui/prompt-import-privatekey-element/prompt-import-privatekey-element.ts
@@ -1,21 +1,22 @@
-import Crypto from "../../providers/crypto/crypto";
-import { CheckboxElement } from "../checkbox-element/checkbox-element";
-import { promptPasswordElement } from "../prompt-password-element/prompt-password-element";
+import Crypto from '../../providers/crypto/crypto';
+import { CheckboxElement } from '../checkbox-element/checkbox-element';
+import { promptPasswordElement } from '../prompt-password-element/prompt-password-element';
 
 export const promptImportPrivatekeyElement = async (
-  ref: HTMLElement
+	ref: HTMLElement
 ): Promise<{
-  privateKey: string;
-  secret: string;
+	privateKey: string;
+	secret: string;
 }> => {
-  const container = document.createElement('div');
-  container.classList.add('prompt-container');
-  ref.after(container);
-  ref.style.display = 'none';
-  container.innerHTML = `
+	const container = document.createElement('div');
+	container.classList.add('prompt-container');
+	ref.after(container);
+	ref.style.display = 'none';
+	container.innerHTML = `
     <div class="prompt__import_file">
       <p>
-        <b>Import Privatekey backup file</b>
+				<b>Import Privatekey backup file</b><br/>
+				& Authenticate with Google to continue
       </p>
       <div class="prompt__import_file__result"></div>
       ${CheckboxElement('Encrypted backup file')}
@@ -26,48 +27,54 @@ export const promptImportPrivatekeyElement = async (
     </div>
   `;
 
-  const buttonImportPrivatekey = container.querySelector('#button__import_privatekey') as HTMLButtonElement;
-  const inputImportFile = container.querySelector('#input__import_file') as HTMLInputElement;
-  const toggleEncription = container.querySelector('#toggle__encription') as HTMLInputElement;
-  
-  const resutl = await new Promise<{
-    privateKey: string;
-    secret: string;
-  }>((resolve, reject) => {
-    buttonImportPrivatekey.addEventListener('click', (e) => {
-      e.preventDefault();
-      inputImportFile.click();
-    });
+	const buttonImportPrivatekey = container.querySelector(
+		'#button__import_privatekey'
+	) as HTMLButtonElement;
+	const inputImportFile = container.querySelector(
+		'#input__import_file'
+	) as HTMLInputElement;
+	const toggleEncription = container.querySelector(
+		'#toggle__encription'
+	) as HTMLInputElement;
 
-    inputImportFile.addEventListener('change', async () => {
-      const file = inputImportFile.files?.[0];
-      if (!file) return;
-      try {
-        const content = await file.text();
-        const isEncrypted = toggleEncription.checked;
-        if (isEncrypted) {
-          const secret = await promptPasswordElement(container, {requestPwd: true});
-          const privateKey = await Crypto.decrypt(secret, content)
-          resolve({secret, privateKey});
-        } else {
-          // request new password to encrypt the private key before store it.
-          const secret = await promptPasswordElement(container);
-          resolve({
-            secret,
-            privateKey: content
-          });
-        }
-        container.remove();
-        ref.style.display = 'block';
-        
-      } catch (error) {
-        reject(error);       
-        container.remove();
-        ref.style.display = 'block';
-      }
-    });
-  });
+	const resutl = await new Promise<{
+		privateKey: string;
+		secret: string;
+	}>((resolve, reject) => {
+		buttonImportPrivatekey.addEventListener('click', e => {
+			e.preventDefault();
+			inputImportFile.click();
+		});
 
-  return resutl;
+		inputImportFile.addEventListener('change', async () => {
+			const file = inputImportFile.files?.[0];
+			if (!file) return;
+			try {
+				const content = await file.text();
+				const isEncrypted = toggleEncription.checked;
+				if (isEncrypted) {
+					const secret = await promptPasswordElement(container, {
+						requestPwd: true
+					});
+					const privateKey = await Crypto.decrypt(secret, content);
+					resolve({ secret, privateKey });
+				} else {
+					// request new password to encrypt the private key before store it.
+					const secret = await promptPasswordElement(container);
+					resolve({
+						secret,
+						privateKey: content
+					});
+				}
+				container.remove();
+				ref.style.display = 'block';
+			} catch (error) {
+				reject(error);
+				container.remove();
+				ref.style.display = 'block';
+			}
+		});
+	});
 
+	return resutl;
 };
