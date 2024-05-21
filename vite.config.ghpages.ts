b8437cb90a519924ade1fad0c6f358573510f72c
FazioNico <contact@nicolasfazio.ch>
2024-05-22 00:53:21 +0200

    refactor: upd build config

--- vite.config.ghpages.ts
+++ vite.config.ghpages.ts
@@ -3,14 +3,18 @@ import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import inject from '@rollup/plugin-inject';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), libInjectCss()],
+	plugins: [nodePolyfills(), wasm(), topLevelAwait(), libInjectCss()],
 	base: '/firebase-web3connect/',
 	build: {
 		target: 'esnext',
 		rollupOptions: {
 			plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
 		}
+	},
+	optimizeDeps: {
+		exclude: ['@syntect/wasm']
 	}
 });
